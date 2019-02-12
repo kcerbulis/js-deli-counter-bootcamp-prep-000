98ea7fb0ad93d09cc2348a2c6458c115653d05c6
reactive-queue-1834 <kcerbulis@gmail.com>
2019-02-12 21:07:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,17 @@ function nowServing(katzDeliLine){
 }
 
 
-
+function currentLine(line){
+  if(!line.length) {
+    return "The line is currently empty.";
+  }
+  var lineNamesandNumbers = [];
+  
+  for(var i=0; i<line.length; i++) {
+    lineNamesandNumbers.push(i+1 + ". "+ line[i]);
+  }
+  console.log("The line is currently: " + lineNamesandNumbers)
+  return "The line is currently: " + lineNamesandNumbers.join(', ');
+}
 
 
