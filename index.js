378d0573d8ec37abd31828788589352de9c92e08
reactive-queue-1834 <kcerbulis@gmail.com>
2019-02-12 21:16:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -42,3 +42,38 @@ function currentLine(line){
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+function currentLine(line){
+  if(!line.length) {
+    return "The line is currently empty.";
+  }
+    var nameAndNumber = [];
+    for (var i = 0; i < line.length; ++i){
+      nameAndNumber.push(i+1 + ". " line[i]);
+    }
+    return 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
